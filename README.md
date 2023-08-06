b4b0d399dc7ecc1e1ef87b239e3dd4584d28bbe3
pieterherman-dev <identity@pieterherman.dev>
2023-08-06 15:42:03 +0200

    Command and DB functionality added

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# notes-cli-app
+# 📝 Notes CLI Application
+
+Notes CLI Application built during the [Workshop Introduction to Node.js, v3](https://frontendmasters.com/workshops/node-js-v3/) by [Scott Moss](https://twitter.com/scotups).
+
+**Participation & Enjoyment By:** [Pieter Herman](https://twitter.com/bepieter), a passionate learner who dived into the Awesome Workshop Introduction to Node.js, v3.
+
+**Led & Inspired By:** The one and only [Scott Moss](https://twitter.com/scotups). His wisdom, insights, and engaging teaching style made this journey an unforgettable learning experience.
+
+## Contents
+
+- [What's This?](#whats-this)
+- [Why?](#why)
+- [My Experience](#my-experience)
+- [Want to Dive Deeper?](#want-to-dive-deeper)
+- [How to use](#how-to-use)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Commands](#commands)
+    - [Options](#options)
+
+## What's This?
+
+A command-line note-taking tool, created during the workshop. Simple, like the best ideas. Made with Node.js, because why not?
+
+## Why?
+
+Because jotting down ideas in the terminal is cool, and because learning through workshops like this one helps shape better developers.
+
+## My Experience
+
+Engaging and insightful. Thanks to Scott Moss's captivating workshop, I've gained valuable insights into Node.js.
+
+## Want to Dive Deeper?
+
+- [Check Here The Workshop Introduction To Node.js, v3 (Unedited Replay)](https://frontendmasters.com/workshops/node-js-v3/)
+- [Learn More About the Man Behind the Magic: Scott Moss](https://github.com/Hendrixer)
+
+## How to use
+
+### Installation
+
+1. Clone the repo
+
+2. `bash npm install && npm link `
+
+### Usage
+
+```bash
+node app.js --help
+```
+
+### Commands
+
+```bash
+  note new <note>     Create a new note
+  note all            get all notes
+  note find <filter>  get matching notes
+  note remove <id>    remove a note by id
+  note web [port]     lanch website to see notes
+  note clean          remove all notes
+```
+
+#### Options
+
+```bash
+      --help     Show help                    [boolean]
+      --version  Show version number          [boolean]
+  -t, --tags     tags to add to the note      [string]
+```
---
+++ src/command.js
@@ -0,0 +1,106 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import {
+  findNotes,
+  getAllNotes,
+  newNote,
+  removeAllNotes,
+  removeNote,
+} from "./notes.js";
+
+const listNotes = (notes) => {
+  notes.forEach(({ id, content, tags }) => {
+    console.log("id: ", id);
+    console.log("tags: ", tags);
+    console.log("content: ", content);
+    console.log("\n");
+  });
+};
+
+yargs(hideBin(process.argv))
+  .command(
+    "new <note>",
+    "Create a new note",
+    (yargs) => {
+      return yargs.positional("note", {
+        type: "string",
+        describe: "The content of the note to create",
+      });
+    },
+    async (argv) => {
+      const tags = argv.tags ? argv.tags.split(",") : [];
+      const note = await newNote(argv.note, tags);
+      console.log("New note! ", note);
+    }
+  )
+  .option("tags", {
+    alias: "t",
+    type: "string",
+    description: "tags to add to the note",
+  })
+  .command(
+    "all",
+    "get all notes",
+    () => {},
+    async (argv) => {
+      const notes = await getAllNotes();
+      listNotes(notes);
+    }
+  )
+  .command(
+    "find <filter>",
+    "get matching notes",
+    (yargs) => {
+      return yargs.positional("filter", {
+        describe:
+          "The search string to use to filter notes by, will be applied to title and content",
+        type: "string",
+      });
+    },
+    async (argv) => {
+      const matches = await findNotes(argv.filter);
+      listNotes(matches);
+    }
+  )
+  .command(
+    "remove <id>",
+    "remove a note by id",
+    (yargs) => {
+      return yargs.positional("id", {
+        describe: "The id of the note to remove",
+        type: "number",
+      });
+    },
+    async (argv) => {
+      const id = await removeNote(argv.id);
+      if (id) {
+        console.log("Note removed: ", id);
+      } else {
+        console.log(id);
+        console.log("Note not found");
+      }
+    }
+  )
+  .command(
+    "web [port]",
+    "lanch website to see notes",
+    (yargs) => {
+      return yargs.positional("port", {
+        describe: "port to bind on",
+        default: 5000,
+        type: "number",
+      });
+    },
+    async (argv) => {}
+  )
+  .command(
+    "clean",
+    "remove all notes",
+    () => {},
+    async (argv) => {
+      await removeAllNotes();
+      console.log("db reseted");
+    }
+  )
+  .demandCommand(1)
+  .parse();
